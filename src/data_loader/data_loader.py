2fe8dd7a6b0939f1fd3b01ffd9386bcde820124c
Julius Pfingsten <julius.pfingsten@gmx.de>
2025-12-04 15:53:34 +0100

    Refactored and created data_loader.py to wrap getter of data loader

---
+++ src/data_loader/data_loader.py
@@ -0,0 +1,38 @@
+import os
+from torch.utils.data import DataLoader
+
+from src.data_loader.dataset import HyperspectralFruitDataset
+from src.data_loader.enums import DatasetSplit 
+from src.data_loader.config import SPLIT_FILES
+
+
+def get_data_loader(CONFIG:dict, split: DatasetSplit, shuffle=False):
+    """
+    Wrapper: Creates a DataLoader for a specific Dataset and assocaited split (train, val, test)
+    Automatically selects the correct JSON file
+    """
+    
+    # Resolve JSON path
+    json_filename = SPLIT_FILES[split]
+    json_path = os.path.join(CONFIG['json_root'], json_filename)
+    
+    print(f"Loading {split.name} data from: {json_filename}")
+
+    dataset = HyperspectralFruitDataset(
+        json_path=json_path,
+        data_root=CONFIG['data_root'],
+        split=split,
+        fruit_type=CONFIG['fruit'],
+        camera_type=CONFIG['camera'],
+        band_strategy=CONFIG['band_strategy'],
+        target_bands=CONFIG['bands'],
+        img_size=CONFIG['img_size']
+    )
+    
+    return DataLoader(
+        dataset, 
+        batch_size=CONFIG['batch_size'], 
+        shuffle=shuffle, 
+        num_workers=CONFIG['num_workers'],
+        pin_memory=True
+    )
