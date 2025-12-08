59f4c9e2a160d830e074d9fd0cade0f3778bf235
Julius Pfingsten <julius.pfingsten@gmx.de>
2025-12-08 18:20:48 +0100

    Changed unfinished get_model for attention cnn test run

--- src/models/models.py
+++ src/models/models.py
@@ -1,6 +1,7 @@
 from src.models.deit_model import DeiTModel
 from src.models.vit_model import VitModel
+from src.models.attention_cnn_model import AttentionCNNModel
 
 
 def get_model(model_type: str, pretrained:bool, num_classes:int, in_channels:int=3):
-    return DeiTModel()
+    return AttentionCNNModel()
