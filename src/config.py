c36eeff04e58c9af87bd94a73fc38d75ec0d80e2
Julius Pfingsten <julius.pfingsten@gmx.de>
2025-12-09 15:21:33 +0100

    Prepare new ruw

--- src/config.py
+++ src/config.py
@@ -1,14 +1,14 @@
 from src.data_loader.utils.enums import FruitType, CameraType
 
 defined_models = {0:"deit", 1: "deit_undist", 2: "vit", 3:"attention_cnn", 4:"lit_spectral_transformer" ,5:"fruiths_net"}
-defined_band_reduction_strategies = {0:"all", 1:"uniform", 2:"average", 3:"gaussian_average"}
+defined_band_reduction_strategies = {0:"all", 1:"uniform", 2:"average", 3:"gaussian_average", 4:"dft"}
 
 
 CONFIG = {
     
     # Names for wandd 
     "project_name": "sapienza_fds_fruit_ripeness",
-    "run_name": "Deit with Gaussian Average", 
+    "run_name": "AttentionCNN with Spetral DFT Band Reduction", 
     
     #TODO NEW RUN: Updated run_name and update selection of model and data
     # Model and data 
@@ -18,12 +18,12 @@ CONFIG = {
     "model_type": defined_models[3],
     "bands": [224, 30, 10, 3][1],
     "band_selection": [None, (700, 1100)][0],
-    "band_reduction": defined_band_reduction_strategies[3],
+    "band_reduction": defined_band_reduction_strategies[4],
     "img_size": [(224, 224), (64, 64)][0],
     
     # Hyperparameters
     "batch_size": 16,
-    "epochs": 30,
+    "epochs": 20,
     "lr": 1e-3,
     "num_workers": 2,
     
