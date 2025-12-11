13948c76a6b88506c0a5a84e0ac3b5fbffceff6a
Bosch <boscherini.2022795@studenti.uniroma1.it>
2025-12-11 21:09:58 +0100

    changed again json file with the first version

--- src/data_loader/utils/config.py
+++ src/data_loader/utils/config.py
@@ -25,7 +25,7 @@ LABEL_MAP = {
 }
 
 SPLIT_FILES = {
-    DatasetSplit.TRAIN: "train_stratified.json", # We only use labeled training data
-    DatasetSplit.VAL: "val_stratified.json",
-    DatasetSplit.TEST: "test_stratified.json"
+    DatasetSplit.TRAIN: "train_only_labeled.json", # We only use labeled training data
+    DatasetSplit.VAL: "val.json",
+    DatasetSplit.TEST: "test.json"
 }
