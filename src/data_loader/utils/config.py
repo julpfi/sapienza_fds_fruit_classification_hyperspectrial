3efdaad1e8e4bade4bd9c34ddd9151beb26bcd22
Bosch <boscherini.2022795@studenti.uniroma1.it>
2025-12-12 14:24:49 +0100

    uploaded new json file (avocado and kiwi VIS)

--- src/data_loader/utils/config.py
+++ src/data_loader/utils/config.py
@@ -25,7 +25,7 @@ LABEL_MAP = {
 }
 
 SPLIT_FILES = {
-    DatasetSplit.TRAIN: "train_avocado_vis.json", # We only use labeled training data
-    DatasetSplit.VAL: "val_avocado_vis.json",
-    DatasetSplit.TEST: "test_avocado_vis.json"
+    DatasetSplit.TRAIN: "train_avocado_kiwi_vis.json", # We only use labeled training data
+    DatasetSplit.VAL: "val_avocado__kiwi_vis.json",
+    DatasetSplit.TEST: "test_avocado_kiwi_vis.json"
 }
