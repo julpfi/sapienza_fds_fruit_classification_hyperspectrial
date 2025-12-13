0a54632a19d6414ebb580b0e44cc7953ca31fe0d
Ludovico Piras <pirasludovico@gmail.com>
2025-12-13 13:51:30 +0100

    training fruits_net with gaussian average

--- src/config.py
+++ src/config.py
@@ -15,10 +15,10 @@ CONFIG = {
     "fruit": FruitType.AVOCADO,
     "camera": CameraType.FX10,  #We will only use the FX10
     "num_classes": 3,
-    "model_type": defined_models[4],
+    "model_type": defined_models[0],
     "bands": [224, 60, 30, 10, 3][1],
     "band_selection": [None, (700, 1100)][0],
-    "band_reduction": defined_band_reduction_strategies[5],
+    "band_reduction": defined_band_reduction_strategies[3],
     "img_size": [(224, 224), (64, 64)][0],
     
     # Hyperparameters
