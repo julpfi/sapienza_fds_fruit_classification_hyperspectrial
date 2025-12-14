3df9a7715c7d82f258b33235d22247114ff726dd
Julius Pfingsten <julius.pfingsten@gmx.de>
2025-12-14 16:40:08 +0100

    Added dft_complex to swin and deit logic

--- src/models/models.py
+++ src/models/models.py
@@ -13,6 +13,8 @@ def get_model(config: dict):
     reduction_strategy = config.get("band_reduction", "all")
     image_size = config.get("img_size", (224, 224))
 
+    in_channels = in_channels * 2 if reduction_strategy == "dft_complex" and model_type not in ['deephs','fruiths_net','hybrid'] else in_channels
+
     # BASELINE PAPER 1
     if model_type == "deephs": 
         return DeephsNet(bands=in_channels)
@@ -24,8 +26,7 @@ def get_model(config: dict):
     
     elif model_type == "lit":
         is_complex = (reduction_strategy == "dft_complex")
-        actual_channels = in_channels * 2 if is_complex else in_channels
-        return LitSpectralTransformer(bands=actual_channels, num_classes=num_classes
+        return LitSpectralTransformer(bands=in_channels, num_classes=num_classes
                                       , is_complex_dft_reduce=is_complex) # Variation dft with real and imag. part -> 1d conv takes 2 channels as input
 
     elif model_type == "deit" and image_size == (224, 224): 
@@ -40,7 +41,6 @@ def get_model(config: dict):
     
     elif model_type == "spec_spat_ft" and reduction_strategy == "dft_complex": 
         crop_size = 16 if image_size == (64, 64) else 48
-        actual_channels = in_channels * 2
-        return SpecSpatFTModel(num_classes=num_classes, in_channels=actual_channels,crop_size=crop_size)
+        return SpecSpatFTModel(num_classes=num_classes, in_channels=in_channels,crop_size=crop_size)
     
     raise ValueError(f"Unsupported model type: {model_type} or incompatible band reduction strategy: {reduction_strategy} for in_channels: {in_channels}")
