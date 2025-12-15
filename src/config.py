94e47db9a5b7d44793a64ac48f3d6419cbc62416
Ludovico Piras <pirasludovico@gmail.com>
2025-12-15 22:19:03 +0100

    training spectralNN with gaussian reduction on old split

--- src/config.py
+++ src/config.py
@@ -18,7 +18,7 @@ CONFIG = {
     "fruit": FruitType.AVOCADO,
     "camera": CameraType.FX10,  #We will only use the FX10
     "num_classes": 3,
-    "model_type": defined_models[6],
+    "model_type": defined_models[5],
     "bands": [224, 30, 10, 3][1],
     "band_selection": [None, (700, 1100)][0],
     "band_reduction": defined_band_reduction_strategies[3],
