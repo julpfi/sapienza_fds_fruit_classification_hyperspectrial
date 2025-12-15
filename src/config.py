85732ab6ab0a2d82f58679ebe40aba8b5a593954
Julius Pfingsten <julius.pfingsten@gmx.de>
2025-12-15 15:29:41 +0100

    Prepare run

--- src/config.py
+++ src/config.py
@@ -11,14 +11,14 @@ CONFIG = {
     
     # Names for wandd 
     "project_name": "sapienza_fds_fruit_ripeness",
-    "run_name": "avocado_all_deit", 
+    "run_name": "avocado_30bands_complex_dft_swin", 
     
     #TODO NEW RUN: Updated run_name and update selection of model and data
     # Model and data 
     "fruit": FruitType.AVOCADO,
     "camera": CameraType.FX10,  #We will only use the FX10
     "num_classes": 3,
-    "model_type": defined_models[4],
+    "model_type": defined_models[3],
     "bands": [224, 30, 10, 3][1],
     "band_selection": [None, (700, 1100)][0],
     "band_reduction": defined_band_reduction_strategies[5],
