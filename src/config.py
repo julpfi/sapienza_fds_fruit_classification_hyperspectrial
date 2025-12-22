4b13c9e852361ee1f6db939caaafd6fb3d6cbd3c
Julius Pfingsten <julius.pfingsten@gmx.de>
2025-12-22 14:28:40 +0100

    Prepare run

--- src/config.py
+++ src/config.py
@@ -11,17 +11,17 @@ CONFIG = {
     
     # Names for wandd 
     "project_name": "sapienza_fds_fruit_ripeness",
-    "run_name": "avocado_30bands_gaussian_hybrid", 
+    "run_name": "avocado_30bands_dft_complex_swin", 
     
     #TODO NEW RUN: Updated run_name and update selection of model and data
     # Model and data 
     "fruit": FruitType.KIWI,
     "camera": CameraType.FX10,  #We will only use the FX10
     "num_classes": 3,
-    "model_type": defined_models[2],
+    "model_type": defined_models[3],
     "bands": [224, 30, 10, 3][1],
     "band_selection": [None, (700, 1100)][0],
-    "band_reduction": defined_band_reduction_strategies[3],
+    "band_reduction": defined_band_reduction_strategies[5],
     "img_size": [(224, 224), (64, 64)][0],   # deit and swin take only 224x224
     
     # Hyperparameters
