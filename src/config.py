d113e921ecc026e88325c21d33b02a5d0f9f1948
Julius Pfingsten <julius.pfingsten@gmx.de>
2025-12-22 14:45:28 +0100

    Prepare run

--- src/config.py
+++ src/config.py
@@ -18,10 +18,10 @@ CONFIG = {
     "fruit": FruitType.KIWI,
     "camera": CameraType.FX10,  #We will only use the FX10
     "num_classes": 3,
-    "model_type": defined_models[3],
+    "model_type": defined_models[2],
     "bands": [224, 30, 10, 3][1],
     "band_selection": [None, (700, 1100)][0],
-    "band_reduction": defined_band_reduction_strategies[5],
+    "band_reduction": defined_band_reduction_strategies[2],
     "img_size": [(224, 224), (64, 64)][0],   # deit and swin take only 224x224
     
     # Hyperparameters
