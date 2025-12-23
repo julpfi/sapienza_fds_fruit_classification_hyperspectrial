f8d84fd10510a636482d038ceafa912220b41006
Julius Pfingsten <julius.pfingsten@gmx.de>
2025-12-23 12:17:04 +0100

    Preapre run

--- src/config.py
+++ src/config.py
@@ -19,9 +19,9 @@ CONFIG = {
     "camera": CameraType.FX10,  #We will only use the FX10
     "num_classes": 3,
     "model_type": defined_models[2],
-    "bands": [224, 30, 10, 3][1],
+    "bands": [224, 30, 10, 3][0],
     "band_selection": [None, (700, 1100)][0],
-    "band_reduction": defined_band_reduction_strategies[5],
+    "band_reduction": defined_band_reduction_strategies[0],
     "img_size": [(224, 224), (64, 64)][0],   # deit and swin take only 224x224
     
     # Hyperparameters
